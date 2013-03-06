0425b6580dc596310c5f261df61726683aea4d9d
Alexander Hughes <alexanderhughes1@gmail.com>
2013-03-06 13:29:55 +0100

    First commit of Tears Of Steel
    Standard robot without any adjustments.

---
+++ src/TearsofSteel.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ * Copyright (c) 2001-2013 Mathew A. Nelson and Robocode contributors
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://robocode.sourceforge.net/license/epl-v10.html
+ *******************************************************************************/
+package sample;
+
+
+import robocode.HitByBulletEvent;
+import robocode.Robot;
+import robocode.ScannedRobotEvent;
+
+
+/**
+ * Tears of Steel - Robot built by Alexander Hughes & Sharmelly Ricardo
+ * <p/>
+ * The moves of this robot are still unknown...
+ *
+ * @author Alexander Hughes/Sharmelly Ricardo
+ */
+public class TearsofSteel extends Robot {
+
+	/**
+	 * MyFirstRobot's run method - Seesaw
+	 */
+	public void run() {
+
+		while (true) {
+			ahead(100); // Move ahead 100
+			turnGunRight(360); // Spin gun around
+			back(100); // Move back 100
+			turnGunRight(360); // Spin gun around
+		}
+	}
+
+	/**
+	 * Fire when we see a robot
+	 */
+	public void onScannedRobot(ScannedRobotEvent e) {
+		fire(1);
+	}
+
+	/**
+	 * We were hit!  Turn perpendicular to the bullet,
+	 * so our seesaw might avoid a future shot.
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnLeft(90 - e.getBearing());
+	}
+}												
+
+				
+	}	
+}				
+	}	
+}				
+	}	
+}
